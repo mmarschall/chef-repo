ddc864fb564af10b84b42c00a7cf86a8baef82ed
Matthias Marschall <mm@agileweboperations.com>
2015-01-02 09:39:20 +0100

    extending 'library' cookbooks through 'application' cookbooks

---
+++ cookbooks/my-ntp/README.md
@@ -0,0 +1,4 @@
+# my-ntp
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/my-ntp/metadata.rb
@@ -0,0 +1,9 @@
+name             'my-ntp'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures my-ntp'
+long_description 'Installs/Configures my-ntp'
+version          '0.1.0'
+
+depends 'ntp'
