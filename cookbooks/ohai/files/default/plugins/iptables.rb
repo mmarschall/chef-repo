fd409d39a9b575d79f2316542694e2e863d75f7d
Matthias Marschall <mm@agileweboperations.com>
2015-01-02 11:18:30 +0100

    Ohai iptables plugin

---
+++ cookbooks/ohai/files/default/plugins/iptables.rb
@@ -0,0 +1,8 @@
+Ohai.plugin(:Iptables) do
+  provides "iptables"
+
+  collect_data(:default) do
+  	iptables Mash.new
+    `iptables -S`.each_line.with_index {|line, i| iptables[i] = line } 
+  end
+end
