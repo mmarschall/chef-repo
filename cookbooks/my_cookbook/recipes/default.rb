ba73bdce3a5a76880b5832a5ea34eac57c01a573
Matthias Marschall <mm@agileweboperations.com>
2016-11-14 06:43:35 +0100

    Test Kitchen recipe

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -3,8 +3,6 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-node.default['my_cookbook']['greeting'] = "Go!"
-
-template '/tmp/greeting.txt' do
-  variables greeting: node['my_cookbook']['greeting']
+file "/tmp/greeting.txt" do
+  content node['my_cookbook']['greeting']
 end
--- cookbooks/my_cookbook/test/recipes/default_test.rb
+++ cookbooks/my_cookbook/test/recipes/default_test.rb
@@ -5,14 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
-end
-
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe file('/tmp/greeting.txt') do
+  its('content') { should match 'Ohai, Chefs!' }
 end
