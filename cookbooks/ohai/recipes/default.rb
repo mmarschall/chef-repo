b39a86bf2a24c8a696038d7c783da3df1e2a1f7f
Matthias Marschall <mm@agileweboperations.com>
2016-11-25 21:55:38 +0100

    ohai custom plugin

--- cookbooks/ohai/recipes/default.rb
+++ cookbooks/ohai/recipes/default.rb
@@ -17,4 +17,4 @@
 # limitations under the License.
 #
 
-Chef::Log.warn('The Ohai cookbook default recipe has no content as of the 4.0 release. See the readme for instructions on using the custom resources.')
+ohai_plugin 'iptables'
